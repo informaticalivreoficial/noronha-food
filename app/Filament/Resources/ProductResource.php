8c136aa4832ec3e5c00e42b309ba706b0873ea7f
informaticalivreoficial <renatomontanari@ymail.com>
2024-07-28 09:41:11 -0300

    product resource update

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -10,10 +10,15 @@ use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Group;
 use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -67,6 +72,49 @@ class ProductResource extends Resource
                     ])
 
                 ])->columnSpan(2),
+
+                Group::make()->schema([
+                    Section::make('Valores')->schema([
+                        TextInput::make('price')
+                            ->label('Valor')
+                            ->numeric()
+                            ->required()
+                            ->prefix('R$')
+                        
+                    ]),
+                    Section::make()->schema([
+                        Select::make('category_id')
+                            ->label('Categoria')
+                            ->required()
+                            ->searchable()
+                            ->preload()
+                            ->relationship('Category', 'name'),
+
+                        Select::make('brand_id')
+                            ->label('Marca')
+                            ->required()
+                            ->searchable()
+                            ->preload()
+                            ->relationship('Brand', 'name')
+                    ]),
+                    Section::make('Status')->schema([
+                        Toggle::make('in_stock')
+                            ->label('em estoque')
+                            ->required()
+                            ->default(true),
+                        Toggle::make('is_active')
+                            ->label('Ativo')
+                            ->required()
+                            ->default(true),
+                        Toggle::make('is_featured')
+                            ->label('destaque')
+                            ->required(),
+                        Toggle::make('on_sale')
+                            ->label('à venda')
+                            ->required(),
+                    ])
+                ])->columnSpan(1)
+
             ])->columns(3);
     }
 
@@ -74,13 +122,28 @@ class ProductResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')->label('Produto')->searchable(),
+                TextColumn::make('category.name')
+                    ->label('Categoria')
+                    ->searchable(),
+                TextColumn::make('brand.name')
+                    ->label('Marca')
+                    ->searchable(),
+                TextColumn::make('price')
+                    ->label('Valor')
+                    ->money('R$'),
+                IconColumn::make('is_featured')->label('Destaque')->boolean(),
+                IconColumn::make('on_sale')->label('Venda')->boolean(),
+                IconColumn::make('in_stock')->label('Estoque')->boolean(),
+                IconColumn::make('is_active')->label('Status')->boolean(),
             ])
             ->filters([
-                //
+                SelectFilter::make('category')->label('Categoria')->relationship('category', 'name'),
+                SelectFilter::make('brand')->label('Marca')->relationship('brand', 'name')
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\EditAction::make()->iconButton(),
+                Tables\Actions\DeleteAction::make()->iconButton(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
