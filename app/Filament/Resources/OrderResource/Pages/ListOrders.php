9a445b956b4dade8502ad0cbc185be20cbbaf484
informaticalivreoficial <renatomontanari@ymail.com>
2024-07-30 20:29:15 -0300

    create stats on orders resource

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
+use App\Filament\Resources\OrderResource\Widgets\OrderStats;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListOrders extends ListRecords
@@ -16,4 +18,30 @@ class ListOrders extends ListRecords
             Actions\CreateAction::make(),
         ];
     }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            OrderStats::class
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            null => Tab::make('All'),
+            'new' => Tab::make('Novo')->query(fn ($query) => $query->where('status', 'new')),
+            'processing' => Tab::make('Processando')->query(fn ($query) => $query->where('status', 'processing')),
+            'shipped' => Tab::make('Enviado')->query(fn ($query) => $query->where('status', 'shipped')),
+            'delivered' => Tab::make('Entregue')->query(fn ($query) => $query->where('status', 'delivered')),
+            'canceled' => Tab::make('Cancelado')->query(fn ($query) => $query->where('status', 'canceled')),
+        ];
+    }
+
+    // protected function getFooterWidgets(): array
+    // {
+    //     return [
+    //         OrderStats::class
+    //     ];
+    // }
 }
