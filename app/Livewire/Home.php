04112f9bf43d84f862d88a130de16d87c7266d9d
informaticalivreoficial <renatomontanari@ymail.com>
2024-08-02 20:12:52 -0300

    categorias cliente update

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -2,12 +2,23 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+
+#[Title('Página Inicial - Noronha Food')]
 
 class Home extends Component
 {
     public function render()
     {
-        return view('livewire.home');
+        $brands = Brand::where('is_active', 1)->get();
+        $categories = Category::where('is_active', 1)->get();
+
+        return view('livewire.home', [
+            'brands' => $brands,
+            'categories' => $categories
+        ]);
     }
 }
